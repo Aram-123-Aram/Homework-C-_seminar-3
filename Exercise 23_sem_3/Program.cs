9cb9a693d621b7e017bc81506f8a7cc88426ddb2
Aram <armsir-8182@mail.ru>
2022-10-28 19:23:07 +0300

    Solving the Exercise 23- showing the table of cubes from 1 to N

--- Exercise 23_sem_3/Program.cs
+++ Exercise 23_sem_3/Program.cs
@@ -1,4 +1,11 @@
 ﻿/* Задача 23:
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27, 
-5 -> 1, 8, 27, 64, 125.
+5 -> 1, 8, 27, 64, 125.*/
+
+Console.Write("Enter any integer number N= ");
+int.TryParse(Console.ReadLine()!,out int N);
+Console.Write($"{N} -> ");
+int i = 1;
+while(i < N+1) {Console.Write($" {Math.Pow(i,3)}, "); i++;}
+
